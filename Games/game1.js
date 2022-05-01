256bee683089fcb6acab3de7e19a28fde61eb707
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-05-01 14:28:11 +0530

    UI/UX Changes To RocPapScis

--- Games/game1.js
+++ Games/game1.js
@@ -2,6 +2,12 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const Bg = document.getElementById('text')
+const compPoints =document.getElementById('computer')
+const youPoints =document.getElementById('you')
+
+let C=0;
+let Y=0;
 
 let userChoice
 let computerChoice
@@ -10,23 +16,46 @@ let result
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e)=>{
     userChoice=e.target.id
     userChoiceDisplay.innerHTML=userChoice
+    colorText()
     generateComputerChoice()
     getResult()
+    
 }))
+
+function colorText(){
+    if (userChoice==='rock'){
+       
+        userChoiceDisplay.style.color='#fc4f05'
+    }
+
+    if (userChoice==='scissor'){
+      
+        userChoiceDisplay.style.color='yellow'
+    }
+
+    if (userChoice==='paper'){
+       
+        userChoiceDisplay.style.color='beige'
+    }
+
+}
 
 function generateComputerChoice(){
     const randomNumber = Math.floor(Math.random()*3)+1
 
     if (randomNumber===1){
         computerChoice='rock'
+        computerChoiceDisplay.style.color='#fc4f05'
     }
 
     if (randomNumber===2){
         computerChoice='scissors'
+        computerChoiceDisplay.style.color='yellow'
     }
 
     if (randomNumber===3){
         computerChoice='paper'
+        computerChoiceDisplay.style.color='beige'
     }
     computerChoiceDisplay.innerHTML=computerChoice
     
@@ -35,31 +64,73 @@ function generateComputerChoice(){
 function getResult(){
     if(computerChoice===userChoice){
         result="It's a Draw!"
+        resultDisplay.style.color='White'
+        Bg.style.backgroundColor='grey'
         
     }
 
     if(computerChoice==='rock'&& userChoice==='paper'){
         result="You Win!"
-       
+        resultDisplay.style.color='#6ae7f7'
+        Bg.style.backgroundColor='#6a6ae6'
+        Y=Y+1
+        Score()
+        C=C
+        
     }
     if(computerChoice==='rock'&& userChoice==='scissors'){
         result="Computer Wins!"
-        
+        resultDisplay.style.color='#ff005d'
+        Bg.style.backgroundColor='#69a600'
+        Y=Y
+        Score()
+        C=C+1
+               
     }
 
     if(computerChoice==='scissors'&& userChoice==='paper'){
         result="Computer Wins!"
+        resultDisplay.style.color='#ff005d'
+        Bg.style.backgroundColor='#69a600'
+        Y=Y
+        Score()
+        C=C+1
+        
     }
     if(computerChoice==='scissors'&& userChoice==='rock'){
         result="You Win!"
+        resultDisplay.style.color='#6ae7f7'
+        Bg.style.backgroundColor='#6a6ae6'
+        Y=Y+1
+        Score()
+        C=C
+        
+        
     }
     if(computerChoice==='paper'&& userChoice==='rock'){
         result="Computer Wins!"
+        resultDisplay.style.color='#ff005d'
+        Bg.style.backgroundColor='#69a600'
+        Y=Y
+        Score()
+        C=C+1
     }
     if(computerChoice==='paper'&& userChoice==='scissors'){
         result="You Win!"
+        resultDisplay.style.color='#6ae7f7'
+        Bg.style.backgroundColor='#6a6ae6'
+        Y=Y+1
+        Score()
+        C=C
+        
     }
     resultDisplay.innerHTML=result
 
 
+}
+
+function Score(){
+    compPoints.innerHTML=C
+    youPoints.innerHTML=Y
+  
 }
