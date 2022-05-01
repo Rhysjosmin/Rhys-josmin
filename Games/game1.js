a298f9f74b63d46284974ef9d1ac0f6ecea04190
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-05-01 11:13:15 +0530

    Added Games
    Added Rock Paper Scissors Basic With a main Game Screen

---
+++ Games/game1.js
@@ -0,0 +1,62 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e)=>{
+    userChoice=e.target.id
+    userChoiceDisplay.innerHTML=userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random()*3)+1
+
+    if (randomNumber===1){
+        computerChoice='rock'
+    }
+
+    if (randomNumber===2){
+        computerChoice='scissors'
+    }
+
+    if (randomNumber===3){
+        computerChoice='paper'
+    }
+    computerChoiceDisplay.innerHTML=computerChoice
+    
+}
+
+function getResult(){
+    if(computerChoice===userChoice){
+        result="It's a Draw!"
+    }
+
+    if(computerChoice==='rock'&& userChoice==='paper'){
+        result="You Win!"
+    }
+    if(computerChoice==='rock'&& userChoice==='scissors'){
+        result="Computer Wins!"
+    }
+
+    if(computerChoice==='scissors'&& userChoice==='paper'){
+        result="Computer Wins!"
+    }
+    if(computerChoice==='scissors'&& userChoice==='rock'){
+        result="You Win!"
+    }
+    if(computerChoice==='paper'&& userChoice==='rock'){
+        result="Computer Wins!"
+    }
+    if(computerChoice==='paper'&& userChoice==='scissors'){
+        result="You Win!"
+    }
+    resultDisplay.innerHTML=result
+
+
+}
