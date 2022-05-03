d191cdbb62f7ae55fadec76c13794d5d5c959356
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-05-03 14:39:44 +0530

    Add files via upload

---
+++ Sub Pages/Papers/paper.js
@@ -0,0 +1,24 @@
+const subject=document.getElementById('subjectName')
+const className=document.getElementById('className')
+const year=document.getElementById('paperYear')
+const address=document.getElementById('link')
+const box =document.getElementById('pdf')
+const paper_index_url="/Paper_Data/New folder/SubjectIndex.json"
+const pdfTemplate=document.getElementById('pdf-template')
+
+let Paper_index
+async function getData(){
+    const card = pdfTemplate.content
+    console.log(card)
+    const response =await fetch(paper_index_url);
+
+    const data = await response.json();
+    subject.innerHTML=data.subject
+    className.innerHTML=data.class
+    year.innerHTML=data.year
+    address.href=data.location
+
+   
+}
+getData()
+
