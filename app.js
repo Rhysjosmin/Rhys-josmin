f85fc9cba7d0df9fa9ee96d2d11aac6e5b8894b9
Rhys-011003 <rod.rhys20031@gmail.com>
2022-07-15 11:28:58 +0530

    gallery

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load',()=>{
     const preload=document.querySelector('.preload');
     preload.classList.add('preload-finish');
     
-})
+});
 
 var loading=[
     'An Apple A Day Makes You An Apple',
@@ -12,34 +12,38 @@ var loading=[
     'Rhys is Going Crazy',
     'The old guy\'s name is Jim'
 
-]
+];
 
 
 
 function newLoad(){
     var randomNumber=Math.floor(Math.random()*(loading.length));
-    document.getElementById('loadingDisp').innerHTML=loading[randomNumber]
+    document.getElementById('loadingDisp').innerHTML=loading[randomNumber];
     
 }
 newLoad();
 
 
 function enlarge(source){
+    if(document.getElementById('imageCont').style.display==='flex'){
+        document.getElementById('imageCont').style.display='none';
+    }else if(document.getElementById('imageCont').style.display===''){
+        document.getElementById('imageCont').style.display='flex';
+    }else if(document.getElementById('imageCont').style.display==='none'){
+        document.getElementById('imageCont').style.display='flex';
+    }
     
-    document.getElementById('imageCont').style.visibility='visible';
+    console.log(document.getElementById('imageCont').style.display)
+    
 
     document.getElementById('image').src=source.src;
     
    
 }
 
-
     
-function close(){
-   
-    document.getElementById('imageCont').style.visibility='hidden';
-}
 
-document.getElementById("close").addEventListener("click", close());
+
+
 
 
