e5c7de2b40a799a52b6113c770dc923d8e269978
Rhys-011003 <rod.rhys20031@gmail.com>
2022-07-31 19:46:47 +0530

    more random phrases

--- app.js
+++ app.js
@@ -11,18 +11,70 @@ var loading=[
     'Chonk Sploby',
     'Rhys is Going Crazy',
     'The old guy\'s name is Jim',
-    'some random phrase'
-
+    'some random phrase',
 ];
+var RAdverbs=[
+    'Randomly',
+    'Racially',
+    'Rapidly',
+    'Rarely',
+    'Rationally',
+    'Readily',
+    'Realistically',
+    'Really',
+    'Reasonably',
+    'Reassuringly',
+    'Recently',
+   'Recklessly',
+    'Refreshingly',
+    'Regardless',
+    'Regionally',
+    'Regretfully',
+    'Regrettably',
+    'Regularly',
+    'Relatively',
+    'Relentlessly',
+    'Reliably',
+   'Religiously',
+    'Reluctantly',
+    'Remarkably',
+    'Remotely',
+    'Repeatedly',
+    'Repetitively'
+]
+var Jing=[
+     'Joking',
+      'Jawing',  
+      'Joying',
+    'Jowing',  'Juking',  'Japing',  'Jewing',  'Jibing',  'Jading', 'Jeeing', 'Jiving',
+'Jigging', 'Joining',  'Jeeping',  'Jeering',  'Jelling',  'Juicing',  'Jogging',  'Jobbing',  'Jerking',  'Jessing', 'Jotting', 'Jesting', 'Jetting', 'Joshing',
+'Jinxing', 'Jibbing ','Jinking','Jouking ','Jilting ','Jazzing','Jauking ','Jumping ','Jacking ','Jolting ','Justing','Junking ','Jurying ','Jamming ','Jambing ','Jailing ','Jutting ','Jarring ','Jagging ','Judging ','Jabbing ','Jugging ','Jauping ','Jingling ','Joisting ','Jollying','Jointing ','Justling','Jangling','Joggling ','Juttying ','Jimmying ','Jettying ','Jaunting ','Jemmying ','Juggling ','Jauncing ','Jousting ','Jumbling ','Jellying ','Jeweling ','Jouncing ','Jiggling ','Jostling ','Juddering ','Junketing ','Joyriding ','Jawboning ','Japanning ','Jewelling ','Jiggering ','Jabbering','Jigsawing ','Jittering ','Jockeying ','Jacketing ','Jargoning ','Jaundicing ','Jugulating ','Jarovizing','Julienning ','Japanizing ','Jaculating ','Justifying ','Javelining ','Jointuring ','Jaywalking ','Jubilating ','Jellifying ','Joypopping ','Jeoparding','Journeying','Jollifying','Jettisoning','Jackknifing ','Jargonizing ','Juxtaposing ','Jackrolling','Jeopardizing ','Jeopardising ','Journalizing ','Jitterbugging','Jackhammering'
+]
 
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function newLoad(){
+    var choose=((Math.random()*2)-1)
     var randomNumber=Math.floor(Math.random()*(loading.length));
-    document.getElementById('loadingDisp').innerHTML=loading[randomNumber];
+    var randomNumberR=Math.floor(Math.random()*(RAdverbs.length));
+    var randomNumberJ=Math.floor(Math.random()*(Jing.length));
+    if(choose>0){
+        document.getElementById('loadingDisp').innerHTML=loading[randomNumber];
+    }
+    if(choose<0){
+    document.getElementById('loadingDisp').innerHTML=RAdverbs[randomNumberR]+' '+Jing[randomNumberJ].trim();    
+    }
     
 }
+
 newLoad();
+
+
+
+
+
 
 
 function enlarge(source){
