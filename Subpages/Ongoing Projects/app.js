bc98742c2d7dd951e366f11fe83c5bdc2c2355bb
Rhys-011003 <rod.rhys20031@gmail.com>
2022-08-08 20:00:36 +0530

    opacity

--- Subpages/Ongoing Projects/app.js
+++ Subpages/Ongoing Projects/app.js
@@ -2,7 +2,7 @@ function parallax(distance){
     const item = document.getElementById('graph')
     const item2 = document.getElementById('main')
     const item3 = document.getElementById('footer')
-    item.style.transform=`translateY(${distance*-.01}px)`
+    item.style.transform=`translateY(${distance*-.005}px)`
     item2.style.transform=`translateY(${distance*.5}px)`
     item3.style.transform=`translateY(${distance*.5}px)`
  
