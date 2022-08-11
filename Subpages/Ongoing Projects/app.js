1d122bace99c8dfd1ee22199e6bdc8d8914553e4
Rhys-011003 <rod.rhys20031@gmail.com>
2022-08-11 13:04:28 +0530

    removed unnecessary words and added mute and unmute buttons

--- Subpages/Ongoing Projects/app.js
+++ Subpages/Ongoing Projects/app.js
@@ -1,33 +1,24 @@
+const cursor=document.getElementById("cursor")
+const cursor2=document.getElementById("cursor2")
+document.addEventListener('mousemove', (event) => {
+    x=event.clientX
+    y=event.clientY
+    parallax(window.scrollY);
+});
+window.addEventListener("scroll",function(){
+    parallax(window.scrollY);
+});
 function parallax(distance){
     const item = document.getElementById('graph')
-    const item2 = document.getElementById('main')
-    const item3 = document.getElementById('footer')
     item.style.transform=`translateY(${(distance*-.1)-100}px)`
-    //item2.style.transform=`translateY(${distance*.5}px)`
-    //item3.style.transform=`translateY(${distance*.5}px)`
-    
     cursor.style.transform=`translateX(${x-40}px) translateY(${y+(window.scrollY)-40}px)`
     cursor2.style.transform=`translateX(${x-30}px) translateY(${y+(window.scrollY)-30}px)`
 }
 
 
-window.addEventListener("scroll",function(){
-    parallax(window.scrollY);
-    
-    
-});
 
-const cursor=document.getElementById("cursor")
-const cursor2=document.getElementById("cursor2")
 
-document.addEventListener('mousemove', (event) => {
-    x=event.clientX
-    y=event.clientY
-    
 
-    parallax(window.scrollY);
 
-	
-});
 
 
--- app.js
+++ app.js
@@ -12,71 +12,20 @@ var loading=[
     'Rhys is Going Crazy',
     'The old guy\'s name is Jim',
     'some random phrase',
+    'Eat your vegetables',
 ];
-var RAdverbs=[
-    'Randomly',
-    'Racially',
-    'Rapidly',
-    'Rarely',
-    'Rationally',
-    'Readily',
-    'Realistically',
-    'Really',
-    'Reasonably',
-    'Reassuringly',
-    'Recently',
-   'Recklessly',
-    'Refreshingly',
-    'Regardless',
-    'Regionally',
-    'Regretfully',
-    'Regrettably',
-    'Regularly',
-    'Relatively',
-    'Relentlessly',
-    'Reliably',
-   'Religiously',
-    'Reluctantly',
-    'Remarkably',
-    'Remotely',
-    'Repeatedly',
-    'Repetitively'
-]
-var Jing=[
-     'Joking',
-      'Jawing',  
-      'Joying',
-    'Jowing',  'Juking',  'Japing',  'Jewing',  'Jibing',  'Jading', 'Jeeing', 'Jiving',
-'Jigging', 'Joining',  'Jeeping',  'Jeering',  'Jelling',  'Juicing',  'Jogging',  'Jobbing',  'Jerking',  'Jessing', 'Jotting', 'Jesting', 'Jetting', 'Joshing',
-'Jinxing', 'Jibbing ','Jinking','Jouking ','Jilting ','Jazzing','Jauking ','Jumping ','Jacking ','Jolting ','Justing','Junking ','Jurying ','Jamming ','Jambing ','Jailing ','Jutting ','Jarring ','Jagging ','Judging ','Jabbing ','Jugging ','Jauping ','Jingling ','Joisting ','Jollying','Jointing ','Justling','Jangling','Joggling ','Juttying ','Jimmying ','Jettying ','Jaunting ','Jemmying ','Juggling ','Jauncing ','Jousting ','Jumbling ','Jellying ','Jeweling ','Jouncing ','Jiggling ','Jostling ','Juddering ','Junketing ','Joyriding ','Jawboning ','Japanning ','Jewelling ','Jiggering ','Jabbering','Jigsawing ','Jittering ','Jockeying ','Jacketing ','Jargoning ','Jaundicing ','Jugulating ','Jarovizing','Julienning ','Japanizing ','Jaculating ','Justifying ','Javelining ','Jointuring ','Jaywalking ','Jubilating ','Jellifying ','Joypopping ','Jeoparding','Journeying','Jollifying','Jettisoning','Jackknifing ','Jargonizing ','Juxtaposing ','Jackrolling','Jeopardizing ','Jeopardising ','Journalizing ','Jitterbugging','Jackhammering'
-]
+
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function newLoad(){
-    var choose=((Math.random()*2)-1)
-    var randomNumber=Math.floor(Math.random()*(loading.length));
-    var randomNumberR=Math.floor(Math.random()*(RAdverbs.length));
-    var randomNumberJ=Math.floor(Math.random()*(Jing.length));
-    if(choose>0){
-        document.getElementById('loadingDisp').innerHTML=loading[randomNumber];
-    }
-    if(choose<0){
-    document.getElementById('loadingDisp').innerHTML=RAdverbs[randomNumberR]+' '+Jing[randomNumberJ].trim();    
-    }
-    
+    var randomNumber=Math.floor(Math.random()*(loading.length));      
+    document.getElementById('loadingDisp').innerHTML=loading[randomNumber];
 }
-
 newLoad();
-
-
-
-
-
-
-
 function enlarge(source){
     if(document.getElementById('imageCont').style.display==='flex'){
         document.getElementById('imageCont').style.display='none';
@@ -98,9 +47,20 @@ function enlarge(source){
 function unmute(){
     
     document.getElementById('showreelVid').muted=false;
+    document.getElementById('showreelVid').volume=0.03;
+    document.getElementById('mute').style.display='block';
+    document.getElementById('unmute').style.display='none';
    
 }
 
+function mute(){
+    
+    document.getElementById('showreelVid').muted=true;
+    document.getElementById('showreelVid').volume=0.01;
+    document.getElementById('mute').style.display='none';
+    document.getElementById('unmute').style.display='block';
+    
+}
 
 
 
