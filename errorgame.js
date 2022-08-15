c72d0386f1b38ffa31980ffdb5f1b5dc2e0f5a6f
Rhys-011003 <rod.rhys20031@gmail.com>
2022-08-15 13:38:01 +0530

    404 game

--- errorgame.js
+++ errorgame.js
@@ -2,8 +2,9 @@ const t=document.getElementById('top');
 const bottom=document.getElementById('bottom');
 const player=document.getElementById('player');
 const floor=document.getElementById('floor');
+const GameArea=document.getElementById('game');
 
-
+let playerPosition=0
 var end=0;
 var ypos=0;
 window.addEventListener('touchstart', function() {
@@ -16,22 +17,61 @@ initialize()
 }})
 
 function initialize(){
-    t.style.transform=`translateY(-8vw)`;
-    bottom.style.transform=`translateY(8vw)`;
-    player.style.transform=`scale(70%) translateY(min(-10vw,-10vh))`;
+    t.style.transform=`translateY(-6vw)`;
+    bottom.style.transform=`translateY(6vw)`;
+    //player.style.transform=`scale(70%) translateY(min(-15vw,-15vh))`;
     floor.style.width='80vw'
-    player.textContent="😊"
-    setTimeout(up,500);
+    player.textContent="😊 "
+    setTimeout(down ,500);
     setTimeout(game,1500);
-    } 
+ 
+    }  
     
 
-function up(){
+function down (){
     player.textContent="☹️"
     player.style.transform=`scale(70%) translateY(min(8vw,8vh))`;   
 }
 
-
-function game(){
-    //Game Code
+function control(e){
+    if(e.keyCode===32){
+        playerPosition=0
+        jumping=false
+        let timerId=setInterval(function(){
+            if(!jumping && playerPosition<60){
+                
+                playerPosition++
+                player.style.transform=`scale(70%)  translateY(${-playerPosition}px)`   
+                         
+            }
+            
+            
+        },15)
+    } 
 }
+
+
+document.addEventListener('keyup',control)
+
+
+function MakeEnemies(){
+    let Enemyposition=82;
+    let randomNum=Math.random()*4000
+    const Enemy =document.createElement('div');
+    Enemy.classList.add('enemy');
+    GameArea.appendChild(Enemy)
+    Enemy.style.left=Enemyposition+'vw'
+    Enemy.textContent='🤡'
+    let timerId=setInterval(function(){
+        if(Enemyposition<12){ 
+            clearInterval(timerId)
+            GameArea.removeChild(Enemy )
+        }
+        Enemyposition-=1;
+        Enemy.style.left=Enemyposition+'vw'
+        console.log(playerPosition)
+        
+    },20);
+    setTimeout(MakeEnemies,randomNum)
+}
+MakeEnemies()
