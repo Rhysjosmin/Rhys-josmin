cb58d23255690c9b26d1216e9ca6b02aaa80ff07
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2023-01-06 14:58:04 +0530

    KenishaPad

---
+++ OtherProjects/Kenisha/app.js
@@ -0,0 +1,91 @@
+
+let container;
+let camera;
+let renderer;
+let scene;
+let pad;
+let CamX=0;
+let CamY=0;
+let CamZ=0;
+let i=0;
+
+function rad(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+
+function init(){
+    container=document.querySelector('.scene');
+
+
+    scene=new THREE.Scene();
+
+    const fov =35;
+    const aspect =container.clientWidth/container.clientHeight;
+    const near =0.01;
+    const far =500;
+
+    const gridHelper = new THREE.GridHelper( 1, 10 );
+
+    scene.add( gridHelper );
+
+
+
+    //Camera Setup
+  camera= new THREE.PerspectiveCamera(fov,aspect,near,far)
+    camera.position.set(1,0.8,0);
+    camera.rotation.set(rad(0),rad(90),rad(0))
+
+    const ambient =new THREE.AmbientLight(0x404040,1);
+    scene.add(ambient)
+    
+    const light=new THREE.DirectionalLight(0XFFaa88,2);
+    light.position.set(0,1,1)
+    scene.add(light);
+
+    renderer=new THREE.WebGLRenderer({antialias:true,alpha:true});
+    renderer.setSize(container.clientWidth,container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    container.appendChild(renderer.domElement);
+
+    const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    
+
+
+
+    //load model
+    let loader =new THREE.GLTFLoader();
+    loader.load("/Pages/Kenisha/Pad.gltf",function(gltf){
+        scene.add(gltf.scene);
+        pad= gltf.scene;
+        renderer.render(scene,camera);
+        animate();
+
+    })
+    
+
+}
+
+function animate(){
+    Math.clz32()
+    i+=.5;
+    requestAnimationFrame(animate);
+    pad.rotation.y+=0.01;
+    pad.children[0].position.y=Math.sin(1.8+i/20)/100;
+    pad.children[1].position.y=.25+Math.sin(2+i/20)/15;
+    pad.children[2].position.y=.25+Math.sin(2.2+i/20)/15;
+    pad.children[3].position.y=.25+Math.sin(2.4+i/20)/15;
+    pad.children[4].position.y=.25+Math.sin(2.8+i/20)/15;
+    pad.children[5].position.y=.25+Math.sin(3+i/20)/15;
+    renderer.render(scene,camera)
+}
+
+init();
+
+
+
+
+
