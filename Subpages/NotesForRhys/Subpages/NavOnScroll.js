21e0c82ab47213414c87093c27df2a028330bfa3
Rhys-011003 <55994011+Rhysjosmin@users.noreply.github.com>
2023-01-24 15:15:39 +0530

    Added Link

--- Subpages/NotesForRhys/Subpages/NavOnScroll.js
+++ Subpages/NotesForRhys/Subpages/NavOnScroll.js
@@ -1,17 +1,17 @@
 const NavOnScroll=document.getElementById('HeroSecondaryContainer')
+
 document.addEventListener('scroll',e=>{
     // console.log(window.scrollY)
-    if(window.scrollY>200 && window.scrollY<400){
-        // console.log('hhd')
-        NavOnScroll.style.opacity=0;
+    if(window.scrollY>100 && window.scrollY<400){
+ 
         NavOnScroll.style.height=0;
     }
     if(window.scrollY>400){
-        NavOnScroll.style.opacity=1;
+       
         NavOnScroll.classList.add('NavOnScroll')
     }
     else{
-        NavOnScroll.style.opacity=1;
+       
         NavOnScroll.classList.remove('NavOnScroll')
     }
 })
