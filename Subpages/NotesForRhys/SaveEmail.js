7fc1ee7737b63ec44dffb2ccfc642ff20a2c4c48
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-27 08:55:07 +0530

    Stuff

--- Subpages/NotesForRhys/SaveEmail.js
+++ Subpages/NotesForRhys/SaveEmail.js
@@ -1,20 +1,15 @@
 // Post a message to a channel your app is in using ID and message text
-async function publishMessage(id, text) {
-    try {
-      // Call the chat.postMessage method using the built-in WebClient
-      const result = await app.client.chat.postMessage({
-        // The token you used to initialize your app
-        token: "xoxb-your-token",
-        channel: id,
-        text: text
-        // You could also use a blocks[] array to send richer content
-      });
-  
-      // Print result, which includes information about the message (like TS)
-      console.log(result);
-    }
-    catch (error) {
-      console.error(error);
-    }
+function publishMessage() {
+             const Name=document.getElementById('Name').value
+             const Email=document.getElementById('Email').value
+
+             console.log(Name)
+             console.log(Email)
+    fetch(`http://127.0.0.1:5000/PostMessage/${"------------------ \n New Email :tada:"}`).then(()=>{
+      fetch(`http://127.0.0.1:5000/PostMessage/Name=${Name}`)
+      fetch(`http://127.0.0.1:5000/PostMessage/Email=${Email}`)
+
+
+    })
   }
-  
+  // publishMessage()
