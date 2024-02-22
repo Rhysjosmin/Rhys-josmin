17d374948d6ee26f3049dce05ca4e52807d2ba19
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-02-22 07:31:08 +0000

    project page

--- portfolio/src/app/Project/second-city-studio/page.tsx
+++ portfolio/src/app/Project/second-city-studio/page.tsx
@@ -5,14 +5,14 @@ const sp_grotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400'] })
 const rbk = Rubik({ subsets: ['latin'], weight: ['300', '400'] })
 
 export default function Page() {
-    return <div className={`${sp_grotesk.className} px-12`}>
+    return <div className={`${sp_grotesk.className} px-12 md:px-20`}>
 
-        <section className="mt-12">
-            <div className="grid grid-cols-2 grid">
-                <h1 className={`${rbk.className}  text-3xl font-bold `}>Second City Studio</h1>
-                <p>Web Front for the second city studio VR destination at birmingham</p>
-                <div>
-                    <p>Fullstack</p>
+        <section className="mt-12 ">
+            <div className="grid grid-cols-5 grid-rows-2 ">
+                <h1 className={`${rbk.className}  text-3xl font-bold row-start-1 col-start-1 col-span-4 `}>Second City Studio</h1>
+                <p className="row-start-2 col-start-1  col-span-4">Web Front for the second city studio VR destination at birmingham</p>
+                <div className="row-start-1 text-neutral-400  text-sm text-end col-start-5 items-end row-span-2 flex flex-col justify-end">
+                    <p className="text-orange-500">Fullstack</p>
                     <p>2023-24</p>
                 </div>
             </div>
