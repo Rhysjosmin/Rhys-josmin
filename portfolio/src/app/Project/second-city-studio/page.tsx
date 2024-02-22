1f9195a76b2bdc8dba6b539d72c95c7c104d32a7
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-02-22 08:15:51 +0000

    stuff

--- portfolio/src/app/Project/second-city-studio/page.tsx
+++ portfolio/src/app/Project/second-city-studio/page.tsx
@@ -1,22 +1,37 @@
+import { MaterialSymbolsLightOpenInNew } from "@/components/icons"
 import { Rubik, Space_Grotesk } from "next/font/google"
 import Image from "next/image"
+import Link from "next/link"
 
-const sp_grotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400'] })
-const rbk = Rubik({ subsets: ['latin'], weight: ['300', '400'] })
+const sp_grotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
+const rbk = Rubik({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800', '900'] })
 
 export default function Page() {
     return <div className={`${sp_grotesk.className} px-12 md:px-20`}>
 
         <section className="mt-12 ">
             <div className="grid grid-cols-5 grid-rows-2 ">
-                <h1 className={`${rbk.className}  text-3xl font-bold row-start-1 col-start-1 col-span-4 `}>Second City Studio</h1>
-                <p className="row-start-2 col-start-1  col-span-4">Web Front for the second city studio VR destination at birmingham</p>
+                <h1 className={`${rbk.className}  text-4xl font-bold row-start-1 col-start-1 col-span-4 `}>Second City Studio</h1>
+                <p className="row-start-2 col-start-1  col-span-4">A website for the second city studio VR destination at birmingham</p>
                 <div className="row-start-1 text-neutral-400  text-sm text-end col-start-5 items-end row-span-2 flex flex-col justify-end">
+
                     <p className="text-orange-500">Fullstack</p>
                     <p>2023-24</p>
                 </div>
             </div>
             <Image src={"/Second City 2.png"} className="rounded-xl mt-8" alt={""} height={1000} width={2000} />
+        </section>
+        <section>
+            <div>
+                <h1>
+                    A website for accessio ......
+                </h1>
+                <p>The Website aims to ....</p>
+            </div>
+            <div>
+                <h1 className=" font-medium mb-2">Website</h1>
+                <Link className="text-indigo-500 " href={'https://secondcitystudio.xyz/'}>https://secondcitystudio.xyz/</Link>
+            </div>
         </section>
     </div>
 }
---
+++ portfolio/src/components/icons.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from "react";
+
+
+
+
+export function MaterialSymbolsLightOpenInNew(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M5.615 20q-.69 0-1.152-.462Q4 19.075 4 18.385V5.615q0-.69.463-1.152Q4.925 4 5.615 4h5.616v1H5.615q-.23 0-.423.192Q5 5.385 5 5.615v12.77q0 .23.192.423q.193.192.423.192h12.77q.23 0 .423-.192q.192-.193.192-.423v-5.616h1v5.616q0 .69-.462 1.152q-.463.463-1.153.463zm4.123-5.03l-.707-.708L18.292 5H14V4h6v6h-1V5.708z"></path></svg>
+    )
+}
